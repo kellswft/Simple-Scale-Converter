a56b725834fcfe3d80f462c98b1a147da7688e88
Cale Fraitzl <112900715+kellswft@users.noreply.github.com>
2022-10-26 07:58:29 -0300

    Update simple-scale-converter.py

--- simple-scale-converter.py
+++ simple-scale-converter.py
@@ -1,11 +1,10 @@
 # Scale calculator/converter (1:48)
-# Scale = real number / scale (48)
+
 real = input('What is the actual length (in.)? ')
 ans = int(real)
 scale = float(ans / 48)
-# To change scale: change 48 to scale value (12, 24, etc.)
-# Add the option of cm as well as inches
 cent = scale * 2.54
 print(scale), print('Inches')
-print(cent), print('Centimeters')
+print(cent), print('Centimetr3s')
+
 # Output in both inches and centimeters @ 1:48 scale of input (inches)
